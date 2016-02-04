748100804acee1fc852868cdcd7e75ce4df8ed2d
George Deng <gndeng@live.com>
2016-02-03 20:39:07 -0800

    #3 sleep fixed

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -35,5 +35,29 @@ class First extends Application {
 		$this->data['authors'] = $authors;
 		$this->render();
 	}
+        
+        function zzz()
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array();
+		$record = $this->quotes->get(1);
+                $this->data = array_merge($this->data, $record);
+		$this->data['authors'] = $authors;
+		$this->render();
+	}
+        
+        function gimme($id)
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array();
+		$record = $this->quotes->get(3);
+                $this->data = array_merge($this->data, $record);
+		$this->data['authors'] = $authors;
+		$this->render();
+	}
 
 }
