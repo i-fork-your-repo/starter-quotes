952bb2f9cc11d48ab855316545225f75204af2ea
George Deng <gndeng@live.com>
2016-02-04 18:55:31 -0800

    commit fix #q7

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,8 +41,10 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
-$route['sleep'] = 'first/zzz';
-$route['show/:num'] = 'first/gimme/3';
+$route['sleep'] = 'first/zzz';  // q3
+$route['show/:num'] = 'first/gimme/3';  // q5
+$route['wise/bingo'] = 'bingo'; //q7
+
 
 
 
